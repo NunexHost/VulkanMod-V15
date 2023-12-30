5977093dd69653ae608536d813dbebbf9cb19f04
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-30 10:49:44 -0300

    Update CompiledSection.java

--- src/main/java/net/vulkanmod/render/chunk/build/CompiledSection.java
+++ src/main/java/net/vulkanmod/render/chunk/build/CompiledSection.java
@@ -1,7 +1,7 @@
 package net.vulkanmod.render.chunk.build;
 
-import com.google.common.collect.Lists;
-import it.unimi.dsi.fastutil.objects.ObjectArraySet;
+import com.google.common.collect.ImmutableList;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import net.minecraft.client.renderer.chunk.VisibilitySet;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -10,37 +10,36 @@ import net.vulkanmod.render.vertex.TerrainRenderType;
 
 import javax.annotation.Nullable;
 import java.util.EnumSet;
-import java.util.List;
 import java.util.Set;
 
 public class CompiledSection {
     public static final CompiledSection UNCOMPILED = new CompiledSection() {
+        @Override
         public boolean canSeeThrough(Direction dir1, Direction dir2) {
             return false;
         }
     };
+
     public final Set<TerrainRenderType> renderTypes = EnumSet.noneOf(TerrainRenderType.class);
-    boolean isCompletelyEmpty = true;
-    final List<BlockEntity> renderableBlockEntities = Lists.newArrayList();
-    VisibilitySet visibilitySet = new VisibilitySet();
+    private boolean isCompletelyEmpty = true;
+    private final List<BlockEntity> renderableBlockEntities = ImmutableList.of(); // Initialize as empty
+    private final VisibilitySet visibilitySet = new VisibilitySet();
     @Nullable
-    TerrainBufferBuilder.SortState transparencyState;
+    private TerrainBufferBuilder.SortState transparencyState;
 
     public boolean hasNoRenderableLayers() {
-        return this.isCompletelyEmpty;
+        return isCompletelyEmpty;
     }
 
-    public boolean isEmpty(TerrainRenderType p_112759_) {
-        return !this.renderTypes.contains(p_112759_);
+    public boolean isEmpty(TerrainRenderType renderType) {
+        return !renderTypes.contains(renderType);
     }
 
     public List<BlockEntity> getRenderableBlockEntities() {
-        return this.renderableBlockEntities;
+        return renderableBlockEntities; // Return immutable list
     }
 
     public boolean canSeeThrough(Direction dir1, Direction dir2) {
-        return this.visibilitySet.visibilityBetween(dir1, dir2);
+        return visibilitySet.visibilityBetween(dir1, dir2);
     }
 }
-
-
